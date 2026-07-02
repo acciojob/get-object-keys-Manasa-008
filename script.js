65555dfa15a95546e6ed7960be654d178333379c
Manasa-008 <manasapenikalapati@gmail.com>
2026-07-02 21:14:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //your JS code here. If required.
 const student={
 	name:"Manasa",
+	age:18,
+	city:"kurnool",
 };
 
 function getKeys(obj){
-	return Object.keys(obj);
+	return Object.keys(obj.name);
 }
 console.log(getKeys(student));
